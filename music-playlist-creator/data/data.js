374ddcd132c9cbed95890b6fefade8e7fd13129d
Joie Ariel Whitmon <joiewhitmon@meta.com>
2025-06-06 09:49:38 -0700

    All features and one strecth feature

---
+++ music-playlist-creator/data/data.js
@@ -0,0 +1,221 @@
+const playlists = [
+   {
+     "playlistID": "001",
+     "playlist_name": "Neo-Soul",
+     "playlist_author": "Jeremiah",
+     "playlist_art": "assets/img/blond.jpg",
+     "likes": 0,
+     "songs": [
+       {
+           title: "Nights",
+           artist: "Frank Ocean",
+           album: "Blonde",
+           duration: "3:30",
+       },
+       {
+           title: "Go Baby",
+           artist: "Cleo Sol",
+           album: "Mother",
+           duration: "4:15",
+       },
+       {
+           title: "Whipped Cream",
+           artist: "Ari Lennox",
+           album: "Shea Butter Baby",
+           duration: "3:42",
+       }
+     ]
+   },
+   {
+     playlistID: "002",
+     "playlist_name": "Classical",
+     "playlist_author": "Amaya",
+     "playlist_art": "assets/img/moon.jpg",
+     "likes": 0,
+     "songs": [
+       {
+           title: "Moonlight Sonata",
+           artist: "Beethoven",
+           album: "Moonlight Sonata",
+           duration: "5:20",
+       },
+       {
+           title: "Canon in D",
+           artist: "Pachelbel",
+           album: "Canon in D",
+           duration: "4:40",
+       },
+       {
+           title: "Clair De Lune",
+           artist: "Debussy",
+           album: "Suite Bergamasque",
+           duration: "5:05",
+       },
+     ]
+   },
+   {
+       playlistID: "003",
+       "playlist_name": "Sad",
+       "playlist_author": "Joie",
+       "playlist_art": "assets/img/wheniwas.jpeg",
+       "likes": 0,
+       "songs": [
+         {
+           title: "When I Was Your Man",
+           artist: "Bruno Mars",
+           album: "Unorthodox Jukebox",
+           duration: "3:34",
+         },
+         {
+           title: "See You Again",
+           artist: "Charlie Puth",
+           album: "Nine Track Mind",
+           duration: "3:50",
+         },
+         {
+           title: "hope ur ok",
+           artist: "Olivia Rodrigo",
+           album: "SOUR",
+           duration: "3:29",
+         }
+       ]
+     },
+     {
+       playlistID: "004",
+       "playlist_name": "Oldies",
+       "playlist_author": "Mya",
+       "playlist_art": "assets/img/micheal.jpg",
+       "likes": 0,
+       "songs": [
+         {
+           title: "Billie Jean",
+           artist: "Michael Jackson",
+           album: "Thriller",
+           duration: "4:54",
+         },
+
+         {
+           title: "Purple Rain",
+           artist: "Prince",
+           album: "Purple Rain",
+           duration: "8:41",
+         },
+         {
+           title: "A House Is Not A Home",
+           artist: "Luther Vandross",
+           album: "Never Too Much",
+           duration: "7:08",
+         }
+       ]
+     },
+     {
+       playlistID: "005",
+       "playlist_name": "Pop Hits",
+       "playlist_author": "Joie",
+       "playlist_art": "assets/img/pharrell.jpeg",
+       "likes": 0,
+       "songs": [
+         {
+           title: "Cruel Summer",
+           artist: "Taylor Swift",
+           album: "Lover",
+           duration: "2:58",
+         },
+         {
+           title: "Happy",
+           artist: "Pharrell Williams",
+           album: "G I R L",
+           duration: "3:53",
+         },
+         {
+           title: "Hey Ya!",
+           artist: "OutKast",
+           album: "Speakerboxxx/The Love Below",
+           duration: "3:55",
+         }
+       ]
+     },
+
+     {
+       playlistID: "006",
+       "playlist_name": "Musicals",
+       "playlist_author": "Arianna",
+       "playlist_art": "assets/img/hamilton.jpeg",
+       "likes": 0,
+       "songs": [
+         {
+           title: "Wait For It",
+           artist: "Leslie Odom Jr",
+           album: "Hamilton",
+           duration: "3:13",
+         },
+         {
+           title: "Seize the Day",
+           artist: "Newsies Cast",
+           album: "Newsies",
+           duration: "4:09",
+         },
+         {
+           title: "Waving Through a Window",
+           artist: "Ben Platt",
+           album: "Dear Evan Hansen",
+           duration: "3:52",
+         }
+       ]
+     },
+     {
+       playlistID: "007",
+       "playlist_name": "Afrobeats",
+       "playlist_author": "Dara",
+       "playlist_art": "assets/img/afrobeats.png",
+       "likes": 0,
+       "songs": [
+         {
+           title: "Last Last",
+           artist: "Burna Boy",
+           album: "Love, Damini",
+           duration: "2:52",
+         },
+
+         {
+           title: "Truth or Dare",
+           artist: "Tyla",
+           album: "Tyla",
+           duration: "3:05",
+         },
+         {
+           title: "KU LO SA",
+           artist: "Oxlade",
+           album: "KU LO SA",
+           duration: "2:16",
+         }
+       ]
+     },
+     {
+       playlistID: "008",
+       "playlist_name": "Rock",
+       "playlist_author": "Theo",
+       "playlist_art": "assets/img/paramore.png",
+       "likes": 0,
+       "songs": [
+         {
+           title: "That's What You Get",
+           artist: "Paramore",
+           album: "Riot!",
+           duration: "3:40",
+         },
+         {
+           title: "Smells Like Teen Spirit",
+           artist: "Nirvana",
+           album: "Nevermind",
+           duration: "5:01",
+         },
+         {
+           title: "Chop Suey!",
+           artist: "System of a Down",
+           album: "Toxicity",
+           duration: "3:30",
+         }
+       ]
+     }
+ ];
--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -2,15 +2,35 @@
 const modal = document.getElementById("playlist-modal");
 const span = document.getElementsByClassName("close")[0];
 
-function openModal(playlistModal) {
-   document.getElementById('modal-playlist-image').src= playlistModal.playlist_art;
-   document.getElementById('modal-title').innerText = playlistModal.playlist_name;
-   document.getElementById('modal-creator').innerText = playlistModal.playlist_author;
-  /* document.getElementById('modalSongImage').innerHTML = */
-   document.getElementsByClassName('song-title').innerText = playlistModal.songs[0].title;
-   document.getElementsByClassName('song-artist').innerText = playlistModal.songs[0].artist;
-   document.getElementsByClassName('album-name').innerText = playlistModal.songs[0].album;
-   document.getElementsByClassName('song-duration').innerText = playlistModal.songs[0].duration;
+let currentPlaylistData = null;
+
+function openModal(playlistData) {
+   // store  current playlist data
+   currentPlaylistData = playlistData;
+
+   document.getElementById('modal-playlist-image').src = playlistData.playlist_art;
+   document.getElementById('modal-title').innerText = playlistData.playlist_name;
+   document.getElementById('modal-creator').innerText = playlistData.playlist_author;
+
+
+   updateModalSongs(playlistData.songs);
+
+   //  event listener  shuffle button
+   const shuffleButton = document.querySelector('.shuffle-button');
+   if (shuffleButton) {
+      // Remove any existing event listeners to prevent duplicates
+      const newShuffleButton = shuffleButton.cloneNode(true);
+      shuffleButton.parentNode.replaceChild(newShuffleButton, shuffleButton);
+
+      // shuffle button event listener
+      newShuffleButton.addEventListener('click', () => {
+         if (currentPlaylistData) {
+            shufflePlaylistSongs(currentPlaylistData);
+         }
+      });
+   }
+
+   // display modal
    modal.style.display = "block";
 }
 
@@ -25,261 +45,54 @@ window.onclick = function(event) {
 
 
 
-//const playlists = document.query
-const playlists = [
-   {
-     "playlistID": "001",
-     "playlist_name": "Neo-Soul",
-     "playlist_author": "Jeremiah",
-     "playlist_art": "assets/img/rnb.jpeg",
-     "likes": 0,
-     "songs": [
-       {
-           title: "Nights",
-           artist: "Frank Ocean",
-           album: "Blonde",
-           duration: "3:30",
-       },
 
-       {
-           title: "Go Baby",
-           artist: "Cleo Sol",
-           album: "IDK",
-           duration: "3:30",
-       },
+// load playlists from local storage
+let savedPlaylists = localStorage.getItem('playlists');
+if (savedPlaylists) {
+   try {
+      // parse the saved playlists
+      const parsedPlaylists = JSON.parse(savedPlaylists);
 
-       {
-           title: "Whipped Cream",
-           artist: "Ari Lennox",
-           album: "IDK",
-           duration: "3:30",
-       }
-     ]
-   },
+      // update  playlists with saved data
+      // keep the like counts between page refreshes
+      parsedPlaylists.forEach((savedPlaylist, index) => {
+         if (index < playlists.length) {
+            playlists[index].likes = savedPlaylist.likes;
+         }
+      });
+   } catch (error) {
+      console.error('Error loading playlists from local storage:', error);
+   }
+}
 
-   {
-     playlistID: "002",
-     "playlist_name": "Classical",
-     "playlist_author": "Amaya",
-     "playlist_art": "assets/img/playlist.png",
-     "likes": 0,
-     "songs": [
-       {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
+// load liked playlists from local storage
+let likedPlaylists = {};
+const savedLikedPlaylists = localStorage.getItem('likedPlaylists');
+if (savedLikedPlaylists) {
+   try {
+      likedPlaylists = JSON.parse(savedLikedPlaylists);
+   } catch (error) {
+      console.error('Error loading liked playlists from local storage:', error);
+   }
+}
 
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
+// function to save playlists to local storage
+function savePlaylists() {
+   try {
+      localStorage.setItem('playlists', JSON.stringify(playlists));
+   } catch (error) {
+      console.error('Error saving playlists to local storage:', error);
+   }
+}
 
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-
-     ]
-   },
-
-   {
-       playlistID: "003",
-       "playlist_name": "Sad",
-       "playlist_author": "Joie",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-
-         {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
-
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
-
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-
-         /*"When I Was Your Man - Bruno Mars",
-         "See You Again - Charlie Puth",
-         "i hope ur ok - olivia rodrigo"*/
-       ]
-     },
-
-     {
-       playlistID: "004",
-       "playlist_name": "Oldies",
-       "playlist_author": "Mya",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-         "Billie Jean - Michael Jackson",
-         "Purple Rain - Prince",
-         "A House Is Not A Home - Luther Vandross"
-       ]
-     },
-
-     {
-       playlistID: "005",
-       "playlist_name": "Classical",
-       "playlist_author": "Amaya",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-
-         {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
-
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
-
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-
-         /*
-         "Moonlight Sonata - Beethoven",
-         "Canon in D - Pachelbel",
-         "Clair de Lune - Debussy"
-         */
-       ]
-     },
-
-     {
-       playlistID: "006",
-       "playlist_name": "Musicals",
-       "playlist_author": "Samantha",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-
-         {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
-
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
-
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-         /*
-         "Wait For It- Leslie Odom Jr",
-         "Seize the Day - Newsies",
-         "Waiting Through the Window - Ben Platt"
-         */
-       ]
-     },
-
-     {
-       playlistID: "007",
-       "playlist_name": "Afrobeats",
-       "playlist_author": "Dara",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-         {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
-
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
-
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-         /*
-         "Last Last - Burna Boy",
-         "Truth or Dare - Tyla",
-         "KU LO SA - Oxlade"
-         */
-       ]
-     },
-
-     {
-       playlistID: "008",
-       "playlist_name": "Rock",
-       "playlist_author": "Theo",
-       "playlist_art": "assets/img/playlist.png",
-       "likes": 0,
-       "songs": [
-         {
-           title: "Moonlight Sonata",
-           artist:"Beethoven",
-           album: "Moonlight Sonata",
-           duration: "3:30",
-       },
-
-       {
-           title: "Canon in D",
-           artist: "Pachelbel",
-           album: "Canon in D",
-           duration: "3:30",
-       },
-
-       {
-           title: "Clair De Lune",
-           artist: "Debussy",
-           album: "IDK",
-           duration: "3:10",
-       },
-
-         /*
-         "That's What You Get - Paramore",
-         "Smells Like Teen Spirit - Nirvana",
-         "Chop Suey! - System of a Down"
-         */
-       ]
-     }
- ]
+// function to save liked playlists to local storage
+function saveLikedPlaylists() {
+   try {
+      localStorage.setItem('likedPlaylists', JSON.stringify(likedPlaylists));
+   } catch (error) {
+      console.error('Error saving liked playlists to local storage:', error);
+   }
+}
 
 const container = document.getElementById('playlist-cards');
 //function that iterates over data.playlists & creates a card for each playlist
@@ -303,13 +116,61 @@ playlists.forEach((playlist) => {
 
       <h3>${playlist.playlist_name}</h3>
       <h4>${playlist.playlist_author}</h4>
-      <p>Songs: ${playlist.songs}</p>
+      <p>Songs: ${playlist.songs.length}</p>
 
       <div class="like-container">
       <span class="like-count">${playlist.likes}</span>
-      <button class="like-btn">👍</button>
+      <button class="like-btn">❤</button>
+      </div>
       `
 
+      // add click event listener to open modal when clicked
+      card.addEventListener('click', () => {
+         openModal(playlist);
+      });
+
+      // get the like button and add a separate click event listener
+      const likeBtn = card.querySelector('.like-btn');
+
+      // checks if this playlist is already liked and set initial button state
+      const isLiked = likedPlaylists[playlist.playlistID] === true;
+      if (isLiked) {
+         likeBtn.style.color = 'red';
+         likeBtn.textContent = '❤';
+      } else {
+         likeBtn.style.color = '';
+         likeBtn.textContent = '❤';
+      }
+
+      likeBtn.addEventListener('click', (event) => {
+         // prevent the click from opening the modal
+         event.stopPropagation();
+
+
+         const isCurrentlyLiked = likedPlaylists[playlist.playlistID] === true;
+
+         if (isCurrentlyLiked) {
+            //unlike
+            playlist.likes--;
+            likedPlaylists[playlist.playlistID] = false;
+            likeBtn.style.color = '';
+            likeBtn.textContent = '❤';
+         } else {
+            // like
+            playlist.likes++;
+            likedPlaylists[playlist.playlistID] = true;
+            likeBtn.style.color = 'red';
+            likeBtn.textContent = '❤';
+         }
+
+         // update the like count display on the card
+         const likeCount = card.querySelector('.like-count');
+         likeCount.textContent = playlist.likes;
+
+         // save updated playlists and liked status to local storage
+         savePlaylists();
+         saveLikedPlaylists();
+      });
 
       //append the card to the container
       container.appendChild(card);
@@ -319,22 +180,211 @@ playlists.forEach((playlist) => {
 
 createPlaylists(playlists);
 
-/*
-//function that dynamically populates the modal
 
-const populateModal = (playlist) => {
-   const modal = document.getElementById('playlist-modal');
-   modal.innerHTML = `
-   <div class="modal-content">
-      <span class="close">&times;</span>
-      <img class="modal-playlist-img" src="${playlist.playlist_art}" alt="${playlist.playlist_name}" width="100">
-      <h2>${playlist.playlist_name}</h2>
-      <h3>${playlist.playlist_author}</h3>
-      <p>Songs: ${playlist.songs}</p>
-   </div>
-   `;
+// Fisher-Yates shuffle algorithm
+function shuffleArray(array) {
+   // copy of the array to prevent changing the og
+   const shuffled = array.slice();
 
+   // start from the last element and swap with a random element before it
+   for (let i = shuffled.length - 1; i > 0; i--) {
+      // random index from 0 to i
+      const j = Math.floor(Math.random() * (i + 1));
+
+      // switch elements at i and j
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+   }
+
+   return shuffled;
 }
 
-populateModal(playlists[0]);
-*/
+
+function shufflePlaylistSongs(playlistData) {
+   const playlistCopy = { ...playlistData };
+
+   // shuffle the songs array
+   playlistCopy.songs = shuffleArray(playlistData.songs);
+
+   // update the modal
+   updateModalSongs(playlistCopy.songs);
+
+   return playlistCopy;
+}
+
+function updateModalSongs(songs) {
+   // clear existing songs
+   const modalSongsContainer = document.querySelector('.modal-songs');
+   modalSongsContainer.innerHTML = '';
+
+   // add all songs from the playlist
+   songs.forEach((song, i) => {
+      // song container
+      const songContainer = document.createElement('div');
+      songContainer.className = 'song-container';
+
+      // song content
+      songContainer.innerHTML = `
+         <div class="song-number">${i + 1}</div>
+         <img class="song-image" src="assets/img/song.png" alt="Song Image">
+         <div class="song-text">
+            <h3 class="song-title">${song.title}</h3>
+            <p class="song-artist">${song.artist}</p>
+            <p class="album-name">${song.album}</p>
+            <p class="song-duration">${song.duration}</p>
+         </div>
+      `;
+
+      // append song to the modal
+      modalSongsContainer.appendChild(songContainer);
+   });
+}
+
+function displayRandomPlaylist() {
+   console.log("displayRandomPlaylist function called");
+
+   try {
+      // check that we are on the featured page by looking at the URL or page elements
+      const isFeaturedPage = window.location.pathname.includes('featured.html') ||
+                            document.querySelector('.featured-playlist-cover') !== null;
+      console.log("Is featured page:", isFeaturedPage);
+
+      // Force update for featured page regardless of URL check
+      const featuredPlaylistCover = document.querySelector('.featured-playlist-cover');
+      const featuredPlaylistTitle = document.querySelector('.featured-playlist-title');
+      const songsRightSection = document.querySelector('.songs-right');
+
+      // If we have the featured page elements, we're on the featured page
+      if (featuredPlaylistCover || featuredPlaylistTitle || songsRightSection) {
+         // Get the elements we need to update
+         const featuredPlaylistCover = document.querySelector('.featured-playlist-cover');
+         const featuredPlaylistTitle = document.querySelector('.featured-playlist-title');
+         const songsRightSection = document.querySelector('.songs-right');
+
+         console.log("Elements found:", {
+            featuredPlaylistCover: !!featuredPlaylistCover,
+            featuredPlaylistTitle: !!featuredPlaylistTitle,
+            songsRightSection: !!songsRightSection
+         });
+
+         // error checkigng make sure the playlists array exists and is not empty
+         if (!playlists || playlists.length === 0) {
+            console.error("No playlists found in the data");
+            return;
+         }
+
+         // select a random playlist
+         const randomIndex = Math.floor(Math.random() * playlists.length);
+         const randomPlaylist = playlists[randomIndex];
+         console.log("Selected random playlist:", randomPlaylist.playlist_name);
+
+         if (featuredPlaylistCover) {
+            featuredPlaylistCover.src = randomPlaylist.playlist_art;
+            featuredPlaylistCover.alt = randomPlaylist.playlist_name;
+         }
+
+         if (featuredPlaylistTitle) {
+            featuredPlaylistTitle.textContent = randomPlaylist.playlist_name;
+         }
+
+         if (songsRightSection) {
+            songsRightSection.innerHTML = '';
+
+            // add songs from the random playlist
+            randomPlaylist.songs.forEach((song, index) => {
+               const songCard = document.createElement('div');
+               songCard.className = 'song-card';
+
+               songCard.innerHTML = `
+                  <p class="song-title">${index + 1}. ${song.title}</p>
+                  <p class="song-artist">${song.artist}</p>
+                  <p class="song-album">${song.album}</p>
+                  <p class="song-duration">${song.duration}</p>
+               `;
+
+               songsRightSection.appendChild(songCard);
+            });
+         }
+      }
+   } catch (error) {
+      console.error("Error in displayRandomPlaylist:", error);
+   }
+}
+
+function filterPlaylists(query) {
+   // convert to lowercase (case sensitve)
+   const searchQuery = query.toLowerCase();
+
+   // filter playlists that match name, author, or song artists
+   const filteredPlaylists = playlists.filter(playlist => {
+      const playlistName = playlist.playlist_name.toLowerCase();
+      const playlistAuthor = playlist.playlist_author.toLowerCase();
+
+      if (playlistName.includes(searchQuery) || playlistAuthor.includes(searchQuery)) {
+         return true;
+      }
+
+      const hasSongArtistMatch = playlist.songs.some(song => {
+         const artistName = song.artist.toLowerCase();
+         return artistName.includes(searchQuery);
+      });
+
+      return hasSongArtistMatch;
+   });
+
+   //  display the matching playlists
+   createPlaylists(filteredPlaylists);
+}
+
+// Function to handle search submission
+function handleSearch() {
+   const searchInput = document.getElementById('search-input');
+   if (searchInput) {
+      const query = searchInput.value.trim();
+      filterPlaylists(query);
+   }
+}
+
+// clear search and reset display
+function clearSearch() {
+   const searchInput = document.getElementById('search-input');
+   if (searchInput) {
+      searchInput.value = '';
+      createPlaylists(playlists); // show all playlists
+   }
+}
+
+// initialize the page when the DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+   console.log("DOM fully loaded");
+
+   // initialize the main page
+   createPlaylists(playlists);
+
+   // display a random playlist on the featured page
+   displayRandomPlaylist();
+
+   // check for featured page elements
+   const featuredElements = document.querySelector('.featured-playlist-cover');
+   if (featuredElements) {
+      console.log("Featured page elements found, forcing display of random playlist");
+      displayRandomPlaylist();
+   }
+
+   // search functionality
+   const searchInput = document.getElementById('search-input');
+   const searchClear = document.getElementById('search-clear');
+
+   if (searchInput && searchClear) {
+      // seearch only when Enter is pressed
+      searchInput.addEventListener('keypress', (event) => {
+         if (event.key === 'Enter') {
+            handleSearch();
+         }
+      });
+
+
+      searchClear.addEventListener('click', () => {
+         clearSearch();
+      });
+   }
+});
